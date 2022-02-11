c0742e8ba686160cebfe899a6c3076a4f638bbee
Riina <riina.kaiv@gmail.com>
2022-02-11 16:04:04 +0200

    Add newer version wb

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+<div id= "first" class="firstsection">
+    <section class="first image section">
+<nav class="level">
+  <p class="level-item has-text-centered">
+    <a class="link is-info">Home</a>
+  </p>
+  <p class="level-item has-text-centered">
+    <a class="link is-info">Menu</a>
+  </p>
+  <p class="level-item has-text-centered">
+    <a class="link is-info">Menu</a>
+  </p>
+  <p class="level-item has-text-centered">
+    <a class="link is-info">Reservations</a>
+  </p>
+  <p class="level-item has-text-centered">
+    <a class="link is-info">Contact</a>
+  </p>
+</nav>
+</section>
+</div>
+
+
+
+
+
+<div class="container">
+    <section class="section">
+        <button class="button is-primary" @click="active=true">Click me</button>
+        <button class="button is-primary" @click="active2=true">Click me2</button>
+        <modal 
+            v-if="active"
+            @close="active=false"
+            url="https://bulma.io/images/placeholders/1280x960.png"
+        >
+        </modal>
+        <modal 
+            v-show="active2"
+            @close="active2=false"
+            url="https://placekitten.com/1280/960"
+        >
+        </modal>
+    </section>
+</div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+    components: { Modal },
+    data(){
+        return {
+            active: false,
+            active2: false
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+
+
+</style>
